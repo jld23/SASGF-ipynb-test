c6be361834dbe0c6daa84137ed7166f98930521d
Jared Dean <jared.dean@sas.com>
2021-01-13 09:58:07 -0500

    add timeout; fix sascfg_personal path

--- binder/sascfg_personal.py
+++ binder/sascfg_personal.py
@@ -4,5 +4,6 @@ SAS_config_names   = ['sasFreeTier']
 sasFreeTier = {'url'     : 'https://cloud.compute.sas.com',
              'context' : 'SAS Free Tier',
              'verify'  : False,
+             'timeout' : 60,
              'options' : ["fullstimer", "memsize=1G"]
              }
